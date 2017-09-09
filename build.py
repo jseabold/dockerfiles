44e8efaf0c40fde62e35b8ad632afe9f503bd48b
Skipper Seabold <jsseabold@gmail.com>
2017-09-09 13:20:42 -0500

    Manual version changes

--- build.py
+++ build.py
@@ -1,14 +1,58 @@
 #! /usr/bin/env python
 
 import glob
-import docker
+import os
+from pprint import pprint
 import sys
+import urllib
+import yaml
 
-from util import get_repo_and_tag
+import docker
+import requests
 
 
+DOCKER_REPO_PREFIX = 'jseabold'
 docker_client = docker.from_env()
 raw_client = docker.APIClient()
+
+
+def get_repo_and_tag(dockerfile):
+    base = os.path.dirname(dockerfile)
+    repo = os.path.join(DOCKER_REPO_PREFIX, base)
+    return base, repo
+
+
+def get_manifest_auth_token(repo):
+    # https://docs.docker.com/registry/spec/auth/token/
+
+    query = urllib.parse.urlencode({
+        'service': 'registry.docker.io',
+        'scope': 'repository:{repo}:pull'.format(repo=repo)
+    })
+
+    login = urllib.parse.urlunsplit((
+        'https',
+        'auth.docker.io',
+        'token',
+        query,
+        ''
+    ))
+
+    token = requests.get(login, json=True).json()["token"]
+    return token
+
+
+def get_tags(repo):
+    token = get_manifest_auth_token(repo)
+    url = "https://registry.hub.docker.com/v2/{repo}/tags/list"
+    tags = requests.get(
+        url.format(repo=repo),
+        headers={
+            'Authorization': 'Bearer {}'.format(token)
+        },
+        json=True
+    ).json()['tags']
+    return tags
 
 
 def build(base, repo_tag):
@@ -20,16 +64,37 @@ def build(base, repo_tag):
             sys.stdout.write(output)
             if line_type != 'stream':
                 sys.stdout.write('\n')
-    return docker_client.images.get(repo_tag)
+
+
+def push(repo_tag):
+    print("Pushing {}".format(repo_tag))
+    stream = docker_client.images.push(repo_tag, stream=True, decode=True)
+    # not going to try to make this nice looking for now
+    for items in stream:
+        pprint(items)
 
 
 def main():
     dockerfiles = glob.glob("*/Dockerfile")
     dockerfiles.sort()
 
+    with open('tags.yaml') as tags:
+        tags = yaml.load(tags)
+
     for dockerfile in dockerfiles:
-        base, repo_tag = get_repo_and_tag(dockerfile)
-        image = build(base, repo_tag)
+        base, repo = get_repo_and_tag(dockerfile)
+        pushed_tags = get_tags(repo)
+        tag = str(tags[base])
+        if tag in pushed_tags:
+            print("Not updating {}".format(repo))
+            continue
+
+        repo_tag = ":".join((repo, tag))
+        print(repo_tag)
+        print("Building")
+        print("Pushing")
+        # build(base, repo_tag)
+        # push(repo_tag)
 
 
 if __name__ == "__main__":
--- util.py
+++
@@ -1,9 +0,0 @@
-import os
-
-DOCKER_REPO_PREFIX = 'jseabold'
-
-def get_repo_and_tag(dockerfile):
-    base = os.path.dirname(dockerfile)
-    tag = os.environ.get("TAG", "latest")
-    repo = os.path.join(DOCKER_REPO_PREFIX, ':'.join((base, tag)))
-    return base, repo
